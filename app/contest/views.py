b5f2997224e8619741edaaafd9e3abc2929457f8
Severus Qin <qinhuapeng425@gmail.com>
2016-04-16 14:53:05 +0800

    update contest list

--- app/contest/views.py
+++ app/contest/views.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-from .. import db
-from peewee import *
 from flask import render_template, redirect, request, url_for, flash
 from . import contest
 from ..models import Contest
 
 @contest.route('/clist')
 def clist_view():
-    return render_template('contest_list.html')
+    clist = None
+    try:
+        clist = Contest.select()
+    except ContestDoesNotExist:
+        pass
+    return render_template('contest_list.html', clist=clist)
+
+@contest.route('/cview/<cid>')
+def contest_view(cid):
+    return render_template('contest_view.html')
