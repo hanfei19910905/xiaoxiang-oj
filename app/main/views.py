7bfc607ff0d6f754794022376945479555e953ef
hanfei19910905 <hanfei19910905@gmail.com>
2016-05-01 17:34:03 +0800

    fix login

--- app/main/views.py
+++ app/main/views.py
@@ -7,13 +7,13 @@ from .forms import LoginForm
 from ..models import User
 from sqlalchemy.orm.exc import NoResultFound
 
+
 @main.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    sess = Session()
     if form.validate_on_submit():
         try:
-            user = sess.query(User).filter(User.email == form.email.data).one()
+            user = User.query.filter_by(email = form.email.data).first()
             if user is not None and user.verify_password(form.password.data):
                 login_user(user, form.remember_me.data)
                 return redirect(request.args.get('next') or url_for('main.index'))
@@ -46,11 +46,8 @@ def logout():
 
 from .. import login_manager
 
+
 @login_manager.user_loader
 def load_user(user_id):
-    try:
-        sess=Session()
-        sess.query(User).filter(User.id == user_id).one()
-    except NoResultFound:
-        return None
+    return User.query.get(int(user_id))
 
