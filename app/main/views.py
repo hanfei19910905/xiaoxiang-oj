9ef8006e6751cf5e0c6d30dbcb33c8b3f556204a
hanfei19910905 <hanfei19910905@gmail.com>
2016-05-03 02:00:52 +0800

    change view.

--- app/main/views.py
+++ app/main/views.py
@@ -12,12 +12,11 @@ from sqlalchemy.orm.exc import NoResultFound
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        try:
-            user = User.query.filter_by(email = form.email.data).first()
-            if user is not None and user.verify_password(form.password.data):
-                login_user(user, form.remember_me.data)
-                return redirect(request.args.get('next') or url_for('main.index'))
-        except NoResultFound:
+        user = User.query.filter_by(email = form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+        else:
             flash('Invalid username or password.')
     return render_template('login.html', form=form)
 
